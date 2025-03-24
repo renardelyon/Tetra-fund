0838f6b688940a95e8c3d4de625ec4ed119719f3
renardelyon <renard.elyon.r@gmail.com>
2025-03-25 02:35:33 +0700

    Handle react frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,36 +1,7 @@
-import { defineConfig } from 'vite';
-import { fileURLToPath, URL } from 'url';
-import environment from 'vite-plugin-environment';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  base: './',
-  plugins: [environment('all', { prefix: 'CANISTER_' }), environment('all', { prefix: 'DFX_' })],
-  envDir: '../',
-  define: {
-    'process.env': process.env
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      define: {
-        global: 'globalThis'
-      }
-    }
-  },
-  resolve: {
-    alias: [
-      {
-        find: 'declarations',
-        replacement: fileURLToPath(new URL('../src/declarations', import.meta.url))
-      }
-    ]
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:4943',
-        changeOrigin: true
-      }
-    },
-    host: '127.0.0.1'
-  }
-});
+  plugins: [react()],
+})
